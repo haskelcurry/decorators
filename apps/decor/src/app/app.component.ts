180d2845743768db19babf817ce2461e12b78013
Markel Tuzynskyi <markel.tuzynskyi@cgm.com>
2022-06-28 11:08:37 +0300

    init

--- apps/decor/src/app/app.component.ts
+++ apps/decor/src/app/app.component.ts
@@ -1,14 +1,4 @@
 import { Component, HostListener } from '@angular/core';
-import { debounce } from 'lodash';
-
-export function Debounce(milliseconds = 500): MethodDecorator {
-  return function(target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
-    console.log(descriptor);
-    
-    const original = descriptor.value;
-    descriptor.value = debounce(original, milliseconds);
-  };
-}
 
 @Component({
   selector: 'test-root',
@@ -17,7 +7,6 @@ export function Debounce(milliseconds = 500): MethodDecorator {
 })
 export class AppComponent {
   @HostListener('document:scroll')
-  @Debounce(250)
   scroll() {
     console.log('scroll');
   }
