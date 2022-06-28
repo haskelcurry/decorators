5a88a5bd2c7fd8c496e6c7df7103c94ac9f27074
Markel Tuzynskyi <markel.tuzynskyi@cgm.com>
2022-06-28 14:11:39 +0300

    solution

--- apps/decor/src/app/app.component.ts
+++ apps/decor/src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component, HostListener } from '@angular/core';
+import { debounce } from 'lodash';
+import { MyService } from './app.service';
+
+export function Debounce(milliseconds = 500): MethodDecorator {
+  return function(target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+    console.log(descriptor);
+    
+    const original = descriptor.value;
+    descriptor.value = debounce(original, milliseconds);
+  };
+}
 
 @Component({
   selector: 'test-root',
@@ -7,7 +18,34 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   @HostListener('document:scroll')
+  @Debounce(250)
   scroll() {
     console.log('scroll');
   }
+
+  constructor(private service: MyService) {}
+}
+
+
+
+
+// from the TS docs
+function first() {
+  console.log("first(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("first(): called");
+  };
+}
+ 
+function second() {
+  console.log("second(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("second(): called");
+  };
+}
+ 
+class ExampleClass {
+  @first()
+  @second()
+  method() {}
 }
