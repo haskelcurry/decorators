dff4b19f50517b73b48565babaaaf19382c33bd1
Markel Tuzynskyi <markel.tuzynskyi@cgm.com>
2022-06-29 13:26:49 +0300

    cleanup

--- apps/decor/src/app/app.component.ts
+++ apps/decor/src/app/app.component.ts
@@ -1,104 +1,13 @@
-import {
-  Component,
-  HostListener, Injector, InjectionToken
-} from '@angular/core';
-import { debounce } from 'lodash';
-import { Log, MyService } from './app.service';
-import { LibConfig } from './lib-config.decorator';
-import { Level } from './logger.service';
-import { OnLangChange, Translatable } from './translation';
+import { Component, HostListener } from '@angular/core';
 
-/* 3 */
-export function Debounce(milliseconds = 500): MethodDecorator {
-  return function (
-    target: any,
-    propertyKey: string | symbol,
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
-    descriptor.value = debounce(originalMethod, milliseconds);
-  };
-}
-
-export type LibConfigInterface = any;
-export const LIB_CONFIG = new InjectionToken<LibConfigInterface>('LIB_CONFIG');
-
-/* 18 */
-// @LibConfig({
-//   version: 2,
-//   theme: 'dark'
-// })
-/* 11 */
-@Translatable()
 @Component({
   selector: 'test-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  /* 17 */
-  providers: [{
-    provide: LIB_CONFIG,
-    useValue: {
-      version: 1,
-      theme: 'dark'
-    }
-  }]
 })
-export class AppComponent implements OnLangChange {
-  /* 2 */
+export class AppComponent {
   @HostListener('document:scroll')
-  /* 6 */
-  @Debounce(250)
-  @Log(Level.Info)
   scroll() {
     console.log('scroll');
   }
-
-  /* 16 */
-  selectedUser = 'some guy';
-
-  ngOnLangChange(lang: string) {
-    console.log('onLocaleChange!', lang);
-  }
-
-  constructor(
-    private service: MyService,
-    public injector: Injector
-  ) {
-    this.service.refreshData('test');
-  }
-}
-
-/* 1 */
-// Order of evaluation and execution
-// from the TS docs
-function first() {
-  console.log('first(): factory evaluated');
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    /* 1.5. Communication Dec <-> Dec */
-    // target.stage = 1;
-    console.log('first(): called'/*, target, propertyKey, descriptor*/);
-  };
-}
-
-function second() {
-  console.log('second(): factory evaluated');
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    // if (target.stage !== 1) throw new Error('@first() is required')
-    // target.stage = 2;
-    console.log('second(): called');
-  };
-}
-
-class ExampleClass {
-  @first()
-  @second()
-  method() {}
 }
--- apps/decor/src/app/app.module.ts
+++ apps/decor/src/app/app.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { SomeComponent } from './some.component';
-import { SharedModule } from './shared.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, SomeComponent],
-  /* 9 */
-  imports: [BrowserModule, SharedModule, /* 20 */ RouterModule.forRoot([{
-    path: 'lazy',
-    loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule)
-  }])],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
