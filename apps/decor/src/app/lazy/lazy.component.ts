7979fa033e2b2a73e649f0680390fefd312846cd
Markel Tuzynskyi <markel.tuzynskyi@cgm.com>
2022-06-29 13:40:13 +0300

    cleanup

--- apps/decor/src/app/lazy/lazy.component.ts
+++ apps/decor/src/app/lazy/lazy.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { LibConfig } from '../lib-config.decorator';
 
-/* 21 */
-@LibConfig({
-  version: 1,
-  theme: 'light',
-  lazy: true
-})
 @Component({
   selector: 'lazy',
   templateUrl: './lazy.component.html',
   styleUrls: ['./lazy.component.scss'],
 })
-export class LazyComponent {
-}
+export class LazyComponent {}
--- apps/decor/src/app/lazy/lazy.module.ts
+++ apps/decor/src/app/lazy/lazy.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SharedModule } from '../shared.module';
 
 import { LazyComponent } from './lazy.component';
 
 @NgModule({
   imports: [
-    SharedModule,
     RouterModule.forChild([{
       path: '',
       component: LazyComponent
